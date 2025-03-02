40e09b31a3ea3b306e43cbb7893712214db24aa6
Isabel Alves <isabel_alves02@hotmail.com>
2025-03-02 13:38:47 -0300

    questII: simple method to compare two numbers and return the biggest, but for this time, I needed to compare inside the method

---
+++ AtivI/src/QuestII/Main.java
@@ -0,0 +1,24 @@
+package QuestII;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite dois números: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(compareAndReturn(a, b));
+    }
+
+    public static String compareAndReturn(int a, int b) {
+        if (a == b) {
+            return "São iguais. Não existe um maior que o outro.";
+        } else {
+            return a > b ? String.format("São diferentes. %d é maior que %d", a,b) : String.format("São diferentes. %d é maior que %d", b,a);
+        }
+    }
+}
