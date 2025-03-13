f9f63b61ab1087aeff20a982a5ba6b7d3cf38b6b
Isabel Alves <isabel_alves02@hotmail.com>
2025-03-12 21:15:34 -0300

    TAD for simulate bankaccount (var in pt_br)

---
+++ TAD/src/BankAccount.java
@@ -0,0 +1,36 @@
+interface IBankAccount {
+    double verSaldo();
+    void depositar(double valor);
+    void sacar(double valor);
+    String atualizarSaldo();
+}
+
+public class BankAccount implements IBankAccount {
+    double saldo;
+
+    @Override
+    public double verSaldo() {
+        return this.saldo;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (this.saldo <= 0 || this.saldo < valor) {
+            System.out.println("Saldo insuficiente");
+        } else {
+          this.saldo -= valor;
+        }
+    }
+
+    @Override
+    public String atualizarSaldo() {
+        return "Seu saldo atualizado é: R$" + this.saldo;
+    }
+
+    public static void main(String[] args) {}
+}
