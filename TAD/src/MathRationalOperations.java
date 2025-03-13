8f55e102ea46736225fc221ae441b28c7cc72fd7
Isabel Alves <isabel_alves02@hotmail.com>
2025-03-12 21:01:03 -0300

    TAD for basic operations with rational numbers

---
+++ TAD/src/MathRationalOperations.java
@@ -0,0 +1,33 @@
+interface IMathRationalOperations {
+    void registerRationalNumbers(int a, int b);
+    int sumNumbers(int a, int b);
+    int multiplyNumbers(int a, int b);
+    Boolean isEqual(int a, int b);
+}
+
+public class MathRationalOperations implements IMathRationalOperations {
+    int a, b;
+
+    @Override
+    public void registerRationalNumbers(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public int sumNumbers(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int multiplyNumbers(int a, int b) {
+        return a*b;
+    }
+
+    @Override
+    public Boolean isEqual(int a, int b) {
+        return a == b;
+    }
+
+    public static void main(String[] args) {}
+}
