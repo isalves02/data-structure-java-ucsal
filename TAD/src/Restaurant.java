81f5c2f3ac47b4009af71563d53d8f0d65d2ef90
Isabel Alves <isabel_alves02@hotmail.com>
2025-03-18 10:40:42 -0300

    TAD for restaurant orders (pt_br)

---
+++ TAD/src/Restaurant.java
@@ -0,0 +1,33 @@
+
+/*
+9 – Crie um TAD para representar um pedido de restaurante.
+O pedido deve conter nome do cliente, itens do pedido, quantidades e valor total.
+ */
+interface IPedido {
+    void novoPedido(String nomeCliente, String[] itensPedido, int[] quantidadesPedido);
+    double valorTotalPedido(double valorItemPedido, int quantidadeItemPedido);
+    void notaFiscal(String nomeCliente, String[] itemPedido, int[] quantidadesPedido, double valorTotal);
+}
+
+class Pedido implements IPedido {
+    private String nomeCliente;
+    private String[] itensPedido;
+    private int[] quantidadesPedido;
+    private double valorTotal;
+
+    @Override
+    public void novoPedido(String nomeCliente, String[] itensPedido, int[] quantidadesPedido) { }
+
+    @Override
+    public double valorTotalPedido(double valorItemPedido, int quantidadeItemPedido) {
+        return valorItemPedido * quantidadeItemPedido;
+    }
+
+    @Override
+    public void notaFiscal(String nomeCliente, String[] itemPedido, int[] quantidadesPedido, double valorTotal) {}
+}
+public class Restaurant {
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido();
+    }
+}
