2d137a4109035f8555497ceeabf908b858d6c345
Isabel Alves <isabel_alves02@hotmail.com>
2025-03-18 10:48:02 -0300

    TAD -- playlist functionalities (pt_br)

---
+++ TAD/src/Playlist.java
@@ -0,0 +1,34 @@
+/*
+10 – Crie um TAD para representar uma playlist de músicas.
+Cada música deve conter título, artista e duração. A playlist deve permitir adicionar, remover e listar músicas.
+* */
+
+interface IMusica {
+    void adicionar(String titulo, String artista, String duracao);
+    void remover(String titulo, String artista, String duracao);
+    void listarMusicas();
+}
+
+class Musica implements IMusica {
+    private String titulo, artista, duracao;
+    private String[][] lista = new String[1000][3];
+
+    @Override
+    public void adicionar(String titulo, String artista, String duracao) { }
+
+    @Override
+    public void remover(String titulo, String artista, String duracao) {}
+
+    @Override
+    public void listarMusicas() {
+        for (var i = 0; i <= lista.length; i++) {
+            for (var j = 0; j <= lista[i].length; j++) {
+                System.out.println(lista[i][j]);
+            }
+        }
+    }
+}
+
+public class Playlist {
+    Musica musica = new Musica();g
+}
