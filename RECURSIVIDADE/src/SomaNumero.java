1a5546bb015385125316b1413e5678707d8ae500
Isabel Alves <isabel_alves02@hotmail.com>
2025-03-25 20:45:58 -0300

    Recursividade: soma de números entre si

--- RECURSIVIDADE/src/SomaNumero.java
+++ RECURSIVIDADE/src/SomaNumero.java
@@ -6,10 +6,26 @@
         2020 = 2 + 0 + 2 + 0
 */
 
-public class SomaAno {
-    // static
-    public static void main(String[] args) {
-        System.out.println("main");
+public class SomaNumero {
+    /*public static int Soma(String numero) {
+        int res = 0;
+        for (int i = 0; i < numero.length(); i++) {
+            int num = numero.charAt(i) - '0';
+            res += num;
+        }
+            System.out.println(res);
 
+        return res;
+    }*/
+
+    public static int SomaHelper(String numero, int in) {
+        if (in == numero.length()) return 0;
+
+        int num = numero.charAt(in) - '0';
+        return num + SomaHelper(numero, in +1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SomaHelper("2202", 0));
     }
 }
