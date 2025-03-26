e73d5358647260c511e4142a8f75e86ba288e14b
Isabel Alves <isabel_alves02@hotmail.com>
2025-03-26 17:14:21 -0300

    Recursividade: soma dos primeiros números naturais

--- RECURSIVIDADE/src/SomaNumNatural.java
+++ RECURSIVIDADE/src/SomaNumNatural.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class SomaNumNatural {
+/*
+* Crie uma função recursiva que calcule a soma dos primeiros N números
+naturais.
+* */
+
+public class SomaNumNatural {
+    public static int SomaPrimeiros(int num) {
+        if (num == 0) return 0;
+
+        return num + SomaPrimeiros(num-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Soma dos primeiros números naturais: " + SomaPrimeiros(6));
+    }
 }
